cecb09cfcb3ab89ace1e912f02bbcb82cd384b70
Maksim Stepanov <stepanov.ms@mail.ru>
2015-04-05 14:23:22 +0300

    Added usage help

--- docker/run.go
+++ docker/run.go
@@ -2,11 +2,17 @@ package docker
 
 import (
   "log"
+  "flag"
+
   "github.com/da4nik/porter/consul"
 )
 
-func RunContainer(serviceName string) {
-  serviceConfig := consul.GetServiceConfig(serviceName)
+func RunContainer() {
+  if flag.Arg(1) == "" {
+    log.Fatal("No service name given")
+  }
+
+  serviceConfig := consul.GetServiceConfig(flag.Arg(1))
 
   log.Println(string(serviceConfig.Value))
 }
---
+++ porter/porter.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+  "os"
+  "strings"
+  "fmt"
+
+  "github.com/da4nik/porter/docker"
+  "flag"
+)
+
+func usage() {
+  var help string
+
+  fmt.Fprint(os.Stdout, "Usage: porter [OPTIONS] COMMAND [arg...]\n\nContainers via consul handle utility \n\nOptions:\n")
+
+  commands := [][]string{
+    {"cleanup", "Cleanup untagged images and exited containers."},
+    {"run", "Run service container"},
+  }
+
+  for _, command := range commands {
+    help += fmt.Sprintf("    %-10.10s%s\n", command[0], command[1])
+  }
+  fmt.Fprintf(os.Stdout, "%s\n", help)
+}
+
+func main() {
+
+  flag.Parse()
+
+  if len(flag.Args()) == 0 {
+    usage()
+    os.Exit(1)
+  }
+
+  switch strings.ToLower(os.Args[1]) {
+  case "cleanup":
+    docker.Cleanup()
+  case "run":
+    docker.RunContainer()
+  default :
+    usage()
+    os.Exit(1)
+  }
+}
