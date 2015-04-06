bf74cd0b31494b0bdead0dc428332880ebde493b
Maksim Stepanov <stepanov.ms@mail.ru>
2015-04-06 21:48:27 +0300

    Tabs to spaces

--- docker/run.go
+++ docker/run.go
@@ -1,18 +1,18 @@
 package docker
 
 import (
-	"flag"
-	"log"
-	"net/http"
+    "flag"
+    "log"
+    "net/http"
 
-	"bytes"
-	"encoding/json"
-	"github.com/da4nik/porter/consul"
-	"io/ioutil"
+    "bytes"
+    "encoding/json"
+    "github.com/da4nik/porter/consul"
+    "io/ioutil"
 )
 
 const (
-	pathCreateContainer = "/containers/create" // POST
+    pathCreateContainer = "/containers/create" // POST
 )
 
 func createContainer(serviceName string) {
@@ -24,42 +24,42 @@ func runContainer(serviceName string) {
 }
 
 func Run() {
-	if flag.Arg(1) == "" {
-		log.Fatal("No service name given")
-	}
+    if flag.Arg(1) == "" {
+        log.Fatal("No service name given")
+    }
 
-	serviceName := flag.Arg(1)
-	serviceConfig := consul.GetServiceConfig(serviceName)
+    serviceName := flag.Arg(1)
+    serviceConfig := consul.GetServiceConfig(serviceName)
 
-	parameters := map[string]interface{}{
-		"Image": serviceName,
-	}
+    parameters := map[string]interface{}{
+        "Image": serviceName,
+    }
 
-	if serviceConfig != nil {
-		// set params
-		log.Println("SETTING CONFIGS")
-		log.Println(serviceConfig.Volumes)
-	}
+    if serviceConfig != nil {
+        // set params
+        log.Println("SETTING CONFIGS")
+        log.Println(serviceConfig.Volumes)
+    }
 
-	params_json, _ := json.Marshal(parameters)
-	log.Println("Container params ", string(params_json))
+    params_json, _ := json.Marshal(parameters)
+    log.Println("Container params ", string(params_json))
 
-	req, err := http.NewRequest("POST", getUrl(pathCreateContainer)+"?name="+serviceName, bytes.NewReader(params_json))
-	if err != nil {
-		log.Fatal("Unable to create request. ", err)
-	}
-	req.Header.Add("Content-type", "application/json")
+    req, err := http.NewRequest("POST", getUrl(pathCreateContainer)+"?name="+serviceName, bytes.NewReader(params_json))
+    if err != nil {
+        log.Fatal("Unable to create request. ", err)
+    }
+    req.Header.Add("Content-type", "application/json")
 
-	client := getHttpClient()
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal("Unable to create container. ", err)
-	}
+    client := getHttpClient()
+    resp, err := client.Do(req)
+    if err != nil {
+        log.Fatal("Unable to create container. ", err)
+    }
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal("Unable to read request body. ", err)
-	}
-	log.Println(resp)
-	log.Println(string(body))
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        log.Fatal("Unable to read request body. ", err)
+    }
+    log.Println(resp)
+    log.Println(string(body))
 }
--- utils/json.go
+++ utils/json.go
@@ -1,15 +1,14 @@
 package utils
 
 import (
-  "log"
-  "encoding/json"
+    "encoding/json"
+    "log"
 )
 
 func ParseJson(data []byte) []interface{} {
-  var parsedJson []interface{}
-  if err := json.Unmarshal(data, &parsedJson); err != nil {
-    log.Fatal("Unable to parse JSON. ", string(data))
-  }
-  return parsedJson
+    var parsedJson []interface{}
+    if err := json.Unmarshal(data, &parsedJson); err != nil {
+        log.Fatal("Unable to parse JSON. ", string(data))
+    }
+    return parsedJson
 }
-
